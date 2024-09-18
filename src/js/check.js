3d8e5317a98893093c171e10790cffba70ae1601
Jintae Song <w.jinttao@gmail.com>
2024-09-18 22:21:22 +0900

    feat(setFlag): 우클릭 깃발 기능 부분 구현

--- src/js/check.js
+++ src/js/check.js
@@ -1,38 +1,38 @@
 // import {renderBoard} from './app.js'
 
+const item = document.querySelectorAll(".item");
 const itemArrForTest = [
   [0,0,1,4,2,4,1,0,0],
   [0,1,2,2,2,1,1,0,0],
   [0,1,4,1,0,0,0,0,0],
   [0,2,3,3,1,1,1,1,0],
-  [0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0],
+  [1,1,1,0,0,0,0,0,0],
+  [1,4,1,0,0,0,0,0,0],
+  [1,1,1,0,0,0,0,0,0],
   [0,0,0,0,0,0,0,0,0],
   [0,0,0,0,0,0,0,0,0],
 ]
 const resolvedItem = Array.from(new Array(9), () => new Array(9).fill(false));
 const blanksAfterClickBlank = [];
+// const imgFlag = 
+const flagRecord = {};
 
-function checkItemValue(event) {
-  const rowItemClicked = event.target.dataset.row;
-  const columnItemClicked = event.target.dataset.column;
-
-  if (itemArrForTest[rowItemClicked][columnItemClicked] === 4) {
+const checkItemValue = function (row, column) {
+  if (itemArrForTest[row][column] === 4) {
     console.log('지뢰 클릭, 게임 끝!');
     return;
   }
   
-  if (itemArrForTest[rowItemClicked][columnItemClicked] > 0 && itemArrForTest[rowItemClicked][columnItemClicked] < 4) {
+  if (itemArrForTest[row][column] > 0 && itemArrForTest[row][column] < 4) {
     console.log('숫자 클릭');
-    confirmResolution(rowItemClicked, columnItemClicked);
+    confirmResolution(row, column);
     return;
   }
 
-  resolveItemBlank(rowItemClicked, columnItemClicked);
-}
+  resolveItemBlank(row, column);
+};
 
-function resolveItemBlank(row, column) {
+const resolveItemBlank = function (row, column) {
   const aboveRow = row - 1;
   const belowRow = row + 1;
   const preColumn = column - 1;
@@ -67,24 +67,47 @@ function resolveItemBlank(row, column) {
   } else {
     return console.log("끝");
   }
-}
+};
 
-function confirmResolution(row, column) {
+const confirmResolution = function (row, column) {
   const WIDTH_SPACE = 9; // import 해결 필요
   const index = row * WIDTH_SPACE + Number(column);
-  const item = document.querySelectorAll(".item")[index];
-
-  item.classList.add("resolved-item");
+  
+  item[index].classList.add("resolved-item");
   resolvedItem[row][column] = true;
 
   if (itemArrForTest[row][column] !== 0) {
-    item.textContent = itemArrForTest[row][column];
+    item[index].textContent = itemArrForTest[row][column];
   }
-}
+};
 
-const item = document.querySelectorAll(".item");
-item.forEach((item) => {
-  item.addEventListener("click", (event) => {
-    checkItemValue(event);
+const setFlag = function (row, column) {
+  const WIDTH_SPACE = 9; // import 해결 필요
+  const index = row * WIDTH_SPACE + Number(column);
+  const imgElement = new Image();
+
+  imgElement.classList.add("img-flag");
+  imgElement.src = 'src/img/flag.svg';
+
+  item[index].appendChild(imgElement);
+  // 좌클릭 불가
+  // 우클릭 2번 하면 풀어주기
+  // 깃발 숫자 카운트
+  // 아이디어: 토글
+};
+ 
+item.forEach((element) => {
+  element.addEventListener("mousedown", (event) => {
+    console.log(event.button);
+    console.log("좌클릭");
+
+    const rowItemClicked = event.target.dataset.row;
+    const columnItemClicked = event.target.dataset.column;
+    // checkItemValue(rowItemClicked, columnItemClicked);
+    setFlag(rowItemClicked, columnItemClicked);
+    // if (event.button === 2) {
+    //   console.log("우클릭");
+    //   return;
+    // }
   })
 });
